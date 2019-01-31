851c32bd48609e08d380df687cffd02e667d3427
Mateus <mateusoliveiracabral@gmail.com>
2019-01-30 23:48:54 -0200

    Segundo Commit + 11 exercicios

--- exercicios/ex078.py
+++ exercicios/ex078.py
@@ -1,25 +1,34 @@
+
 lista = []
+menorl = []
+maiorl = []
 for i in range (0,5):
 
     num = int(input(f"Digite um valor para a posicao {i}"))
+    lista.append(num)
     if i == 0:
         menor=num
         maior=num
-        lista.append(num)
+
 
     else:
         if num > maior:
             maior = num
-            lista.append(num)
+            maior =
+
 
         if menor > num:
             menor = num
-            lista.append(num)
+
 
 
 print(f"O maior valor digitado foi {maior} nas posicoes")
+print(lista.index(maior))
 
 
 print(f"O menor valor digitado foi {menor} nas posicoes")
 
-print(num)
+for i in range(0, lista.count(menor)):
+    print(lista.index(menor))
+
+print(lista)
